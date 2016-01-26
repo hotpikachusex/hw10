57de1f5f49c10ded8efee0a34331c1e55023b9e1
Viktor Pfaffenrot <pfaffenrot@linux-81en.suse>
2016-01-26 21:29:13 +0100

    done

--- diffusion.cxx
+++ diffusion.cxx
@@ -17,14 +17,14 @@ void step(double* const u1,  double* const u0,  const double dt,
 int main(){
 
   const double tEnd = 5 ;
-  const double D = 1;
+  const double D = 0.1;
 
   const int N  = 200;
   const double xmin = -20;
   const double xmax = 20;
   const double dx = (xmax-xmin)/(N-1) ;
 
-  double dt = dx;
+  double dt = dt;
   double t = 0;
   const int Na = 10;
   const int Nk = int(tEnd/Na/dt);
@@ -44,8 +44,14 @@ int main(){
   for(int i=1; i<=Na; i++)
   {
    for(int j=0; j<Nk; j++){
-
-
+    //hier swap
+    
+       step(u1, u0, dt, dx, D, N);
+       
+       h = u1;
+       u1 = u0;
+       u0 = h;
+       t += dt;
    }
    strm.str("");
    strm << "u_" << i;
@@ -63,7 +69,10 @@ void step(double* const f1, double* const f0,
           const double dt, const double dx,
           const double D, const int N)
 {
-
+    
+    for(int i = 1; i < N-1; i++){
+        f1[i] = D * dt / (dx * dx) * (f0[i+1] - 2.0 * f0[i] + f0[i-1]) + f0[i];
+    }
 }
 //-----------------------------------------------
 void initialize(double* const u0, const double dx,
